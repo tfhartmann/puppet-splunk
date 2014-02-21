88aa038d1bf602b0948a7190ddff5392c352f846
Tim Hartmann <tfhartmann@gmail.com>
2014-02-21 19:30:14 +0000

    Hey I'm starting to curl the API - Yay

--- lib/puppet/provider/splunk_search_peers/test.rb
+++ lib/puppet/provider/splunk_search_peers/test.rb
@@ -4,4 +4,24 @@ require 'net/http'
 require 'net/https'
 require 'openssl'
 
-puts 'Hello World'
+
+#curl --get -d "output_mode=json" -k -u admin https://localhost:8089/services/search/distributed/peers
+
+def GET(path)
+  request = Net::HTTP::Get.new(path)
+  authorize(request)
+  connection.request(request)
+end
+
+def connection
+  return @connection if defined?(@connection)
+  @connection = Net::HTTP.new("localhost", 8089)
+  @connection.use_ssl = true
+  @connection.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  @connection
+end
+
+def authorize(request)
+  request.basic_auth 'admin', 'changeme'
+end
+puts GET('/services/search/distributed/peers')
